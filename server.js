a0c3a0a16a0d1ed2eceac22f090b88bdb8b5612d
brihanPenayo <116458646+brihanPenayo@users.noreply.github.com>
2023-02-05 19:44:10 -0300

    ejercicio core finalizado

--- server.js
+++ server.js
@@ -3,21 +3,55 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-class Product {
+class Usuario {
     constructor() {
-        this.name = faker.random.words();
-        this.price = "$" + faker.commerce.price();
-        this.department = faker.commerce.department();
+        this._id = faker.random.numeric(3);
+        this.firstName = faker.name.firstName();
+        this.lastName = faker.name.lastName();
+        this.phoneNumber = faker.phone.number();
+        this.email = faker.internet.email();
+        this.password = faker.internet.password(8);
     }
 }
 
-console.log(new Product());
+class Empresa {
+    constructor() {
+        this._id = faker.random.numeric(3);
+        this.name = faker.company.name();
+        this.address = [
+            { street: faker.address.street() },
+            { city: faker.address.city() },
+            { state: faker.address.state() },
+            { postalCode: faker.address.zipCode() },
+            { country: faker.address.country() }
+        ]
+    }
+}
+
+console.log(new Empresa());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.json("HolaMundo")
+    res.json(`Bienvenido! Esto es una API Falsa`)
+})
+
+app.get("/users/new", (req, res) => {
+    res.json(new Usuario())
+})
+
+app.get("/companies/new", (req, res) => {
+    res.json(new Empresa())
+})
+
+app.get("/user/company", (req, res) => {
+    res.json([
+        {
+            Usuario: new Usuario(),
+            Company: new Empresa()
+        }
+    ])
 })
 
 app.listen(port);
